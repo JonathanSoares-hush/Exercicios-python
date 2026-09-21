72f26171d6be6c5c21de6d1196360475c655a19f
JonathanSoares-hush <jonathan_f_soares@estudante.sesisenai.org.br>
2026-09-21 19:08:44 +0000

    Parte 1 dos exercicios de python

---
+++ parte 1 - varíaveis/Exercício3.py
@@ -0,0 +1,7 @@
+## Peça o raio de um círculo e calcule a área. Use 3.14159 como valor de pi.
+
+pi = 3.14159
+raio = float(input("Digite o valor do raio do círculo: "))
+
+area = pi * (raio ** 2)
+print("A área do círculo é: " + str(area))
---
+++ parte 1 - varíaveis/Exercício4.py
@@ -0,0 +1,5 @@
+## Peça uma temperatura em graus Celsius e converta para Fahrenheit. A fórmula é F = C × 9 / 5 + 32.
+
+c = float(input("Digite a temperatura em graus Celsius: "))
+fahrenheit = c * 9 / 5 + 32
+print("A temperatura em Fahrenheit é: " + str(fahrenheit))
